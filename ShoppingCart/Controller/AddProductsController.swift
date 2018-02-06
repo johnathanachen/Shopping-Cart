f5af3bd9680bd751d7e9019fb7ea26b6c9f2b478
Johnathan Chen <johnathanchenn@outlook.com>
2018-02-05 22:56:12 -0800

    UI Design from json

--- ShoppingCart/Controller/AddProductsController.swift
+++ ShoppingCart/Controller/AddProductsController.swift
@@ -10,25 +10,62 @@ import UIKit
 import CoreData
 
 class AddProductsController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
+    var products = [Products]()
+    
     @IBOutlet weak var imageLabel: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     
+    let images = [#imageLiteral(resourceName: "ultraboost"),#imageLiteral(resourceName: "prophere"),#imageLiteral(resourceName: "eqt"),#imageLiteral(resourceName: "AlphaBounce"),#imageLiteral(resourceName: "Dame4"),#imageLiteral(resourceName: "Primeknit")]
+    
     override func viewDidLoad() {
-        
+        readJson()
     }
     @IBAction func addToCartButton(_ sender: UIButton) {
     }
     
+    func readJson() {
+        do {
+            let path = Bundle.main.path(forResource: "products", ofType: "json")
+            let jsonURL = URL(fileURLWithPath: path!)
+            let jsonDecoder = JSONDecoder()
+            let savedJSONData = try Data(contentsOf: jsonURL)
+            let jsonDisplay = try jsonDecoder.decode([Products].self, from: savedJSONData)
+            
+            self.products = jsonDisplay
+            
+        } catch {print(error)}
+    }
+    
+    struct Products: Codable {
+        var title: String
+        var image: String
+        var price: String
+    }
+    
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        return self.products.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-  
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath) as! ProductCell
+        let product = products[indexPath.row]
+        
+        cell.imageLabel.image = UIImage(named: product.image)
+        cell.titleLabel.text = product.title
+        cell.priceLabel.text = product.price
+        
+        return cell
     }
     
+ 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
     }
---
+++ ShoppingCart/View/ProductCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductCell.swift
+//  ShoppingCart
+//
+//  Created by Johnathan Chen on 2/5/18.
+//  Copyright © 2018 JCSwifty. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+    
+    @IBOutlet weak var imageLabel: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+
+}
